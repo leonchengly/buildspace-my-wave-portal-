c51c5fec99a419b510875e51c3f4836c53035b4c
Li Yang Cheng <chengliyang@gmail.com>
2022-11-12 11:36:24 +0800

    Storing messages from users on the blockchain - Done

--- scripts/run.js
+++ scripts/run.js
@@ -14,10 +14,6 @@ const main = async () => {
   let waveTxn = await waveContract.wave("A message!");
   await waveTxn.wait(); // Wait for the transaction to be mined
 
-  const [_, randomPerson] = await hre.ethers.getSigners();
-  waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
-  await waveTxn.wait(); // Wait for the transaction to be mined
-
   let allWaves = await waveContract.getAllWaves();
   console.log(allWaves);
 };
